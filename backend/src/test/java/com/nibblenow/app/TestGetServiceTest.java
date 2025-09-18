3fb8f06983b7eece149182d5559d62cf70c201b4
Niko Komninos <nk@nikokomninos.com>
2025-09-18 15:48:58 -0400

    refactor: update unit testing templates

--- backend/src/test/java/com/nibblenow/app/TestGetServiceTest.java
+++ backend/src/test/java/com/nibblenow/app/TestGetServiceTest.java
@@ -1,16 +1,32 @@
 package com.nibblenow.app;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.nibblenow.app.services.TestGetService;
 
 class TestGetServiceTest {
 
+  private TestGetService s;
+  private String result; 
+
+  @BeforeEach
+  public void setUp() {
+    this.s = new TestGetService();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    this.s = null;
+    this.result = null;
+  }
+
   @Test
   public void testMethod() {
-    TestGetService s = new TestGetService();
-    String result = s.testGet();
-    assertEquals("Hello!", result);
+    this.result = this.s.testGet();
+    assertEquals("Hello!", this.result);
   }
 }
--- backend/src/test/java/com/nibblenow/app/TestPostServiceTest.java
+++ backend/src/test/java/com/nibblenow/app/TestPostServiceTest.java
@@ -1,16 +1,32 @@
 package com.nibblenow.app;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.nibblenow.app.services.TestPostService;
 
 class TestPostServiceTest {
 
+  private TestPostService s;
+  private String result;
+
+  @BeforeEach
+  public void setUp() {
+    this.s = new TestPostService();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    this.s = null;
+    this.result = null;
+  }
+
   @Test
   public void testMethod() {
-    TestPostService s = new TestPostService();
-    String result = s.testPost("Niko");
-    assertEquals("Hello, Niko!", result);
+    this.result = this.s.testPost("Niko");
+    assertEquals("Hello, Niko!", this.result);
   }
 }
