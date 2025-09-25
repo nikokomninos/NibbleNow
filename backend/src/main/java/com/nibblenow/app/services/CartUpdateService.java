e8dbaa348af4dceafd65f8dd6657ad060181ebf9
Christopher Sciortino <34723997+RedstoneWiz@users.noreply.github.com>
2025-09-25 18:58:16 -0400

    Added the addItemToCart Function

--- backend/src/main/java/com/nibblenow/app/services/CartUpdateService.java
+++ backend/src/main/java/com/nibblenow/app/services/CartUpdateService.java
@@ -1,8 +1,11 @@
 
 package com.nibblenow.app.services;
 
+import java.util.ArrayList;
+
 import com.nibblenow.app.MenuItem;
 import com.nibblenow.app.Order;
+import com.nibblenow.app.User;
 
 /**
  * CartUpdateService:
@@ -11,12 +14,36 @@ import com.nibblenow.app.Order;
  * 
  * @author Nikolaos Komninos, Katarina Liedbeck, Christopher Sciortino
  */
-public class CartUpdateService {
+public class CartUpdateService
+{
+  /**
+   * @param user - this is a User object of the user that should have the MenuItem added to their cart.
+   * @param item - this is a MenuItem that should be added to the users cart.
+   * @return - a MenuItem that has been added to the user's cart or null if the user or item
+   *           that was passed was null as then the item was not added to any user's cart.
+   */
+  public MenuItem addItemToCart(User user, MenuItem item)
+  {
+    // If no user or no menu item was passed, return null as the function can't run:
+    if(user == null || item == null)
+    {
+      return(null);
+    }
 
-  public MenuItem addItemToCart() {
-    return null;
+    // Create an ArrayList of menu items and get the cart from the user:
+    ArrayList<MenuItem> newCart = user.getCart();
+
+    // Add the new item to our updated ArrayList:
+    newCart.add(item);
+
+    // Update the user's cart to include the new added item:
+    user.setCart(newCart);
+
+    // Return the item that was added:
+    return(item);
   }
 
+  
   public MenuItem removeItemFromCart() {
     return null;
   }
