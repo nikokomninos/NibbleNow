b0143b83d9df344ddd527976d93fd9c24017133b
Christopher Sciortino <34723997+RedstoneWiz@users.noreply.github.com>
2025-09-25 18:44:12 -0400

    Updated Populate Database

--- backend/src/main/java/com/nibblenow/app/App.java
+++ backend/src/main/java/com/nibblenow/app/App.java
@@ -62,41 +62,62 @@ public class App {
      * Populates the user database with sample users
      * We used ChatGPT to generate some fake user data
      * for us to help us test our program.
+     * 
+     * Prompt: "Can you create 30 fake user following the given format. The roles are
+     * Restaurant Owner, Customer, Delivery Driver. Usernames and passwords must
+     * be between 5 and 25 characters, contain no spaces or special characters. 
+     * Passwords must contain atleast one uppercase letter. Here is the format:
+     * Database.USERS.add(new User("username", "password", "role", cart));"
      */
-    public static void populateUserDatabase() {
-        Database.USERS.add(new User("testUser", "testPassword", "Customer"));
-        Database.USERS.add(new User("testOwner", "testPassword", "Restaurant Owner"));
-        Database.USERS.add(new User("alexRider99", "Fast@Food123", "Customer"));
-        Database.USERS.add(new User("bellaChef22", "Cook&Roll9", "Restaurant Owner"));
-        Database.USERS.add(new User("charlieDrive88", "Speed*Way7", "Delivery Driver"));
-        Database.USERS.add(new User("daniDine55", "Forks#Up8", "Customer"));
-        Database.USERS.add(new User("eddieEats21", "Munch!Time4", "Customer"));
-        Database.USERS.add(new User("felixRunner", "Zoom&Deliver1", "Delivery Driver"));
-        Database.USERS.add(new User("graceGrillz", "Hot$Grillz2", "Restaurant Owner"));
-        Database.USERS.add(new User("hannahTaste", "Yum@Plate3", "Customer"));
-        Database.USERS.add(new User("ianDash90", "Move#It5", "Delivery Driver"));
-        Database.USERS.add(new User("jessieSpice", "Spicy*Queen7", "Restaurant Owner"));
-        Database.USERS.add(new User("kyleZoomer", "Fast$Feet8", "Delivery Driver"));
-        Database.USERS.add(new User("lunaLunch", "Lunch#Box5", "Customer"));
-        Database.USERS.add(new User("mattBurger", "Burg@King9", "Restaurant Owner"));
-        Database.USERS.add(new User("ninaQuick", "Run&Drop2", "Delivery Driver"));
-        Database.USERS.add(new User("owenEater", "Full$Belly6", "Customer"));
-        Database.USERS.add(new User("paulaPan", "Fry!Master3", "Restaurant Owner"));
-        Database.USERS.add(new User("quentinDash", "Zip&Zap7", "Delivery Driver"));
-        Database.USERS.add(new User("rachelBites", "Chomp@Chomp1", "Customer"));
-        Database.USERS.add(new User("samSizzle", "Grill#Time2", "Restaurant Owner"));
-        Database.USERS.add(new User("tonyDrive", "Rush*Rush5", "Delivery Driver"));
+    public static void populateUserDatabase()
+    {
+        ArrayList<MenuItem> cart = new ArrayList<MenuItem>();
+        Database.USERS.add(new User("testUser", "testPassword", "Customer", cart));
+        Database.USERS.add(new User("testOwner", "testPassword", "Restaurant Owner", cart));
+        
+        Database.USERS.add(new User("jakeThompson22", "FastFoodLover1", "Customer", cart));
+        Database.USERS.add(new User("mariaLopez", "StrongPass2", "Restaurant Owner", cart));
+        Database.USERS.add(new User("driverDylan99", "DriveSafe7", "Delivery Driver", cart));
+        Database.USERS.add(new User("samanthaGreen", "GreenKitchen3", "Restaurant Owner", cart));
+        Database.USERS.add(new User("billyCook88", "BurgerTimeA1", "Restaurant Owner", cart));
+        Database.USERS.add(new User("customerJenny", "BestChoiceB9", "Customer", cart));
+        Database.USERS.add(new User("alexDriver", "ZoomZoomQ4", "Delivery Driver", cart));
+        Database.USERS.add(new User("tinaTurner", "MusicMealsZ2", "Customer", cart));
+        Database.USERS.add(new User("nathanCooks", "PanFryTuna5", "Restaurant Owner", cart));
+        Database.USERS.add(new User("zoeMiller23", "TastyTreatK6", "Customer", cart));
+        Database.USERS.add(new User("carlosDriver", "QuickRunU8", "Delivery Driver", cart));
+        Database.USERS.add(new User("stephBaker", "OvenReadyW3", "Restaurant Owner", cart));
+        Database.USERS.add(new User("happyCustomer", "GreatMealY7", "Customer", cart));
+        Database.USERS.add(new User("graceParks", "DinnerTimeX5", "Restaurant Owner", cart));
+        Database.USERS.add(new User("davidDeliver", "SpeedyG99", "Delivery Driver", cart));
+        Database.USERS.add(new User("michaelChef", "PlateMasterA2", "Restaurant Owner", cart));
+        Database.USERS.add(new User("lindaJones", "FoodFanZ8", "Customer", cart));
+        Database.USERS.add(new User("harryDriver88", "FastTrackR6", "Delivery Driver", cart));
+        Database.USERS.add(new User("restaurantEva", "KitchenProU3", "Restaurant Owner", cart));
+        Database.USERS.add(new User("oliviaCustomer", "MealPassT4", "Customer", cart));
+        Database.USERS.add(new User("noahSmith", "YummyDishK1", "Customer", cart));
+        Database.USERS.add(new User("driverKyle", "DeliverQuickE7", "Delivery Driver", cart));
+        Database.USERS.add(new User("lucasGrill", "HotGrillM2", "Restaurant Owner", cart));
+        Database.USERS.add(new User("emmaFoodie", "SnackTimeF5", "Customer", cart));
+        Database.USERS.add(new User("chefTony88", "MasterCookG3", "Restaurant Owner", cart));
+        Database.USERS.add(new User("danielDrive", "RideOnX9", "Delivery Driver", cart));
+        Database.USERS.add(new User("bellaCustomer", "FoodLoverV6", "Customer", cart));
+        Database.USERS.add(new User("ethanKitchen", "PrepItUpB2", "Restaurant Owner", cart));
+        Database.USERS.add(new User("driverNina", "RunOrderH4", "Delivery Driver", cart));
+        Database.USERS.add(new User("kevinEats", "MealBoxZ1", "Customer", cart));
+
+
         System.out.println("User database populated!");
     }
 
     /**
      * populateMenu
-     * 
      * Populates the menu of the restaurant '500 Degrees'
      * We used ChatGPT to generate some fake food data
      * for us to help us test our program.
      */
-    public static void populateMenu() {
+    public static void populateMenu()
+    {
         Database.RESTAURANTS.put("500 Degrees", new Restaurant("500 Degrees", "Pizzeria"));
         ArrayList<MenuItem> menu = new ArrayList<MenuItem>();
         menu.add(new MenuItem("Regular Pizza", "A classic pizza with tomato sauce and cheese."));
